ffd303c51b1700f176d1d1fbe29c2824db44c5b6
LUCAS FERNANDES DE OLIVEIRA <randury@A202-10L.imd.local>
2019-10-14 20:31:06 -0300

    resolvido

--- aula11_ex06.c
+++ aula11_ex06.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 
-int main(){
+/*Faça  um  programa  que  receba  três  números
+ e  mostre-­os  em  ord  crescente.  */
 
+int main(){
+	// recebendo numeros do usuario
+	printf("Insira 3 inteiros: ");
+	int num[3];
+	scanf("%d", &num[0]);
+	scanf("%d", &num[1]);
+	scanf("%d", &num[2]);
+
+	// ordem utilizada para validar com facilidade
+	int ord[5] = {0, 1, 2, 0, 1};
+
+	// verificando se existem nums iguais
+	// sera utilizado o mesmo padrao para toda validacao
+	for (int i=0; i<3; i++){
+
+		// se determinado numero for igual a algum outro no vetor
+		if (num[ord[i]] == num[ord[i+1]] || num[ord[i]] == num[ord[i+2]]){
+			printf("Numeros iguais\n");
+			return 1;
+		}
+	}
+
+	printf("\nOrdem crescente:\n");
+
+
+	// imprimir o menor
+
+	for (int i=0; i<3; i++){
+		// se determinado numero for menor que todos no vetor
+		if (num[ord[i]] < num[ord[i+1]] && num[ord[i]] < num[ord[i+2]])
+			printf("%d ", num[ord[i]]);
+	}
+
+	// imprimir o do meio
+
+	for (int i=0; i<3; i++){
+		// se determinado numero for menor que algum 
+		// numero e maior que outro numero no vetor
+		if (num[ord[i]] < num[ord[i+1]] && num[ord[i]] > num[ord[i+2]])
+			printf("%d ", num[ord[i]]);
+
+		// mesmo caso de cima porem com sinais invertidos
+		else if (num[ord[i]] > num[ord[i+1]] && num[ord[i]] < num[ord[i+2]])
+			printf("%d ", num[ord[i]]);
+	}
+
+	// imprimir o maior
+
+	for (int i=0; i<3; i++){
+		// se determinado numero for maior que todos no vetor
+		if (num[ord[i]] > num[ord[i+1]] && num[ord[i]] > num[ord[i+2]])
+			printf("%d ", num[ord[i]]);
+	}
+
+
+	// quebra de linha final
+	printf("\n");
 
     return 0;
 }
