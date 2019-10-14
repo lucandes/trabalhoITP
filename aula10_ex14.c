e67d3758a24e88c16a85888ecf2b41702f793e55
LUCAS FERNANDES DE OLIVEIRA <randury@A202-10L.imd.local>
2019-10-14 19:05:27 -0300

    resolvido

--- aula10_ex14.c
+++ aula10_ex14.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
-int main(){
+/*Escreva  um  programa  que  leia  um  numero  
+inteiro  positivo  N  e  em  seguida  imprima  N  
+linhas  do  chamado  Triangulo  de  Floyd.  
+Dica:  utilizar  laços  de  repetição  aninhados.
+*/
 
+int main(){
+	// variavel que recebe o inteiro do usuario
+	int n;
+	printf("Insira um inteiro: ");
+	scanf("%d", &n);
+
+	// variavel que representa os numeros impressos
+	int nums = 1;
+
+	// linha representa o espaco onde serao impressos
+	for (int linha=1; linha<=n; linha++){
+
+		// imp representa os numeros que foram impressos
+		// ao reiniciar o loop imp volta a ser 0
+		for (int imp=0; imp<linha; printf("%d ", nums++))
+			// cada vez que um numero for impresso imp += 1 
+			imp++;
+
+		// uma nova linha sera criada ao fim do loop
+		printf("\n");
+	}
 
     return 0;
 }
