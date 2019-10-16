b261f9a875e72e8ea2b5da5e1b3dffb85247ee99
LUCAS FERNANDES DE OLIVEIRA <randury@A202-02L.imd.local>
2019-10-16 18:33:07 -0300

    resolvido

--- aula16_ex08.c
+++ aula16_ex08.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+/* Faça  um  algoritmo  que  receba  um  número  
+inteiro  positivo  n  e  calcule  o  seu  fatorial,  
+n!.  O  fatorial  de  um  número  é  calculado  
+pela  multiplicação  desse  número  por  todos  
+os  seus  antecessores  até  chegar  ao  número  1
+(o  zero  (0)  é  excluído). */
+
 int main(){
+	// n sera o inteiro recebido e m o fatorial
+	int n, m;
+	// recebendo inteiro do usuario
+	scanf("%d", &n);
+	// a variavel m recebe o valor de n
+	m = n;
+
+	// sera repetido ate que n = 1
+	while (n != 1)
+		m *= (--n);
+
+	// impressao do resultado
+	printf("%d\n", m);
 
 
     return 0;
