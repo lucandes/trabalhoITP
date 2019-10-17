da800752cf0ae5ca4a216843dacfeb9b7498dbed
LUCAS FERNANDES DE OLIVEIRA <randury@A202-02L.imd.local>
2019-10-16 21:25:24 -0300

    resolvido

--- aula16_ex16.c
+++ aula16_ex16.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+/*   Escreva  um  programa  que  leia  um  numero  
+inteiro  positivo  N  e  criei  uma  função  que  
+imprima   N   linhas   do   chamado   Triangulo   
+de   Floyd. */
+
+void Floyd(int N);
 
 int main(){
 
+    int N;
 
-    return 0;
+    scanf("%d", &N);
+    Floyd(N);
+
+return 0;
+}
+
+void Floyd(int N){
+
+    int linha;
+    int c = 0; //incrementa os valores
+
+    // para cada linha em N
+    for(linha=1; linha<=N; linha++){
+        // para cada numero impresso
+       for(int j=1; j<=linha; j++){
+            c++;
+            printf("%d\t",c);
+        }
+       printf("\n");
+    }
+
 }
