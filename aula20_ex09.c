e1fa390e660e34b914674892c29fd00ffbdc58b0
lucandes <51387908+lucandes@users.noreply.github.com>
2019-10-16 21:18:51 -0300

    resolvido

--- aula20_ex09.c
+++ aula20_ex09.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
+double Converter_De_kmh_Para_ms(double vel){
+   
+    return vel / 3.6 ; //Esta função retorna a conversão de Km/h por M/s.  
+}
+
+double Converter_de_Ms_para_kmh(double vel){
+   
+    return vel * 3.6; //Esta função retorna a conversão de M/s por Km/h.
+     
+}
+
+int main() {
+   
+    double vel; //velosidade
+
+char tipo; //Tipo de velocidade
+
+printf("Informe qual a velocidade:\n");
+scanf("%lf", &vel); //Recebendo a velocidade.
+printf("Informe qual o tipo velocidade:\n");
+scanf("%s", &tipo); //Recebendo qual o tipo da velocidade.
 
 
-    return 0;
+if(tipo == 'K' ){ //Teste para recebimento de velocida em km/h.
+   vel=Converter_De_kmh_Para_ms(vel); // Atribuindo a velocidade para receber o valor retornado pela função Converter_De_kmh_Para_ms
+   printf("%.2f M/s", vel); //Exibindo o valor convertido em M/s  
+  } else if(tipo == 'M' ){ // Teste para recebimento de velocida em M/s.
+   vel = Converter_de_Ms_para_kmh(vel); // Atribuindo a velocidade para receber o valor retornado pela função Converter_de_Ms_para_kmh
+   printf("%.2f Km/h", vel); // Exibindo o valor convertido em Km/h
+  }
+ 
+return 0;
 }
