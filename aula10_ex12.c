433d7387700699e3b47433d5b42253df347fe2ad
lucandes <51387908+lucandes@users.noreply.github.com>
2019-10-17 11:02:47 -0300

    resolvido

--- aula10_ex12.c
+++ aula10_ex12.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*   Escreva   um   programa   que   receba   como   entrada   o   valor   do   saque   realizado   pelo  
+/*  Escreva   um   programa   que   receba   como   entrada   o   valor   do   saque   realizado   pelo  
 cliente  de  um  banco  e  retorne  quantas  notas  de  cada  valor  serão  necessárias  para  
 atender  ao  saque  com  a  menor  quantidade  de  notas  possível.  Serão  utilizadas  notas  
 de  100,  50,  20,  10,  e  5  reais.  
