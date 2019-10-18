2ce50a20dce1ac853a05d088b8034a1900821a70
Lucas <lucasfern99@gmail.com>
2019-10-18 12:48:27 -0300

    resolvido

--- aula26.2_ex13.c
+++ aula26.2_ex13.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <string.h>
+
+int soma(int *A, int *B, int X, int Y, int *resultado){
+    int i;
+   	
+   	// caso os tamanhos sejam diferentes
+    if(X != Y)
+        return 0;
+
+    // caso sejam iguais
+    else {
+    	// leitura do vetor A
+    	printf("\nInsira os membros do vetor A:\n");
+        for(int i=0; i<X; i++){
+        	scanf("%d", &A[i]);
+    	}
+   		
+   		// leitura do vetor B
+   		printf("\nInsira os membros do vetor B:\n");
+    	for(int i=0; i<Y; i++){
+        	scanf("%d", &B[i]);
+    	}
+    	
+    	// a soma dos numeros de cada vetor sera inserida no vetor resultado
+        for(i=0; i<X; i++){
+            resultado [i] = A[i] + B[i];
+        }
+    }
+    return 1;    
+}
 
 int main(){
+	// declaracao das variaveis de tamanho
+    int X, Y;
+    // funcao recebera o retorno da funcao soma
+    int funcao;
+   	
+   	// recebendo entradas do usuario
+    printf("Digite o Tamanho do primeiro vetor\n");
+    scanf("%d", &X);
+    printf("Digite o Tamanho do segundo vetor\n");
+    scanf("%d", &Y);
+   	
+   	// declarando vetores com os tamanhos inseridos pelo usuario
+    int A[X], B[Y];
+    int resultado[X];
 
+    funcao = soma(A, B, X, Y, resultado);
+   	
+   	// caso o retorno da funcao seja diferente de 0
+    if(funcao != 0){
 
+    	// impressao do resultado
+    	printf("\nVetor resultado:\n");
+        for(int i=0; i<X; i++){
+            printf("%d ", resultado[i]);
+        }
+    }
+
+    // imprime o retorno da funcao
+    printf("\n\nRetorno da funcao: %d\n", funcao);
+   
     return 0;
 }
